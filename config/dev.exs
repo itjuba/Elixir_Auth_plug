aa2c11f7214ce2358bcee5be8791e91e83815afb
Attig Nadjib <49757658+itjuba@users.noreply.github.com>
2021-10-11 00:38:32 +0100

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :simpleAPI, SimpleAPI.Repo,
   username: "juba",
-  password: "Nadjib11051963",
+  password: "",
   database: "simpleapi_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
